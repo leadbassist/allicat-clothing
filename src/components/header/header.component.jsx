2b8b90798d1477c09e77b99382e8271e5be8d638
leadbassist <leadbassist@users.noreply.github.com>
2020-08-24 23:21:01 -0400

    created user and cart sagas for sign in, sign up, and sign in with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,10 +10,11 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors.js';
 import { selectCurrentUser } from '../../redux/user/user.selectors.js';
+import { signOutStart } from '../../redux/user/user.actions.js';
 
 import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
   <HeaderContainer>
     <LogoContainer to="/">
       <Logo className='logo'>
@@ -29,7 +30,7 @@ const Header = ({ currentUser, hidden }) => (
       </OptionLink>
       {
         currentUser ? (
-        <OptionLink as='div' className='option' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
+        <OptionLink as='div' className='option' onClick={ signOutStart }>SIGN OUT</OptionLink>
          ) : (
         <OptionLink to='/signin'>SIGN IN</OptionLink>
          )}
@@ -46,4 +47,8 @@ const mapStateToProps = createStructuredSelector({
   hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+  signOutStart: () => dispatch(signOutStart())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
